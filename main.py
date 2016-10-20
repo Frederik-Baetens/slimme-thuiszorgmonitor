37e5ecf21fcf02a97863f434864a4f641e57d9e9
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-10-21 01:25:31 +0200

    bug met switch leds gefixt, nieuwe bug met reform_list

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import pyb
 ## CONSTANTEN ##
 FREQ = 128              #frequentie waaraan gemeten wordt
 LENGTH_ELEMENT = 12     #constanten voor omzetten naar binair
-LENGTH_MESSAGE = 128
+LENGTH_MESSAGE = 128    #lengte totale boodschap voor aes encryptie
 
 
-## PIN-NUMMERS ## --> deze pins zijn tijdelijk, kan nog gewijzigd worden
-PO_PIN_NB = 'X4'            #nummer van de pulse-oximeter pin
-ECG_PIN_NB = 'X3'           #nummer van de electrocardiogram pin
-PRESSURE_PIN_NB = 'X5'      #nummer van de druksensor pin
-PIN_RED_LED_NB = 'X1'       #X1: naam van de pin, naam van deze pin moet nog aangepast worden
-PIN_INFRARED_LED_NB = 'X2'  #X2: naam van de pin, naam van deze pin moet nog aangepast worden
+## PIN-NUMMERS ##
+PO_PIN_NB = 'X19'           #nummer van de pulse-oximeter pin
+ECG_PIN_NB = 'X20'          #nummer van de electrocardiogram pin
+PRESSURE_PIN_NB = 'X21'     #nummer van de druksensor pin
+PIN_RED_LED_NB = 'X9'       #X9: Naam pin
+PIN_INFRARED_LED_NB = 'X10' #X10: Naam pin
 
 
 ## PINNEN AANMAKEN ##
@@ -24,9 +24,14 @@ pressure_pin = pyb.ADC(PRESSURE_PIN_NB)
 ##pin_infrared_led = pyb.Pin(PIN_INFRARED_LED_NB, Pin.OUT_PP) # deze code werkt nog niet
 
 
+## INITIALISEREN ##
+current_led = pyb.LED(2)    # deze moeten vervangen worden door pin_red_led en pin_infrared_led
+next_led = pyb.LED(3)       # voor het testen worden de LED's op het bordje gebruikt
+
+
 ## FUNCTIES ##
 def reform_list(list):
-    # returns a message from a list with 3 elements in binary that is 128 characters long
+    # returns a message from a list in binary that is 128 characters long
     for i in range(len(list)):
         list[i] = bin(list[i])[2:]
     for i in range(len(list)):
@@ -45,9 +50,7 @@ def read_and_send():
     value_ecg = ecg_pin.read()
     value_po = po_pin.read()
     value_pressure = pressure_pin.read()
-    switch_leds(current_led, next_led)
-    # LEDS verwisselen
-    current_led, next_led = next_led, current_led  
+    switch_leds() 
 
     list = [value_ecg, value_po, value_pressure]
     
@@ -58,36 +61,30 @@ def read_and_send():
     #BLUETOOTH: send(encrypted_message)
 
 
-def switch_leds(led_on, led_off):
-    # led_on is the LED that is on before running this function (this will be turned off)
-    # led_off is the LED that is off before running this function (this will be turned on)
-    led_on.off()
-    led_off.on()
+def switch_leds():
+    global current_led
+    global next_led
+    current_led, next_led = next_led, current_led
 
+"""
+def encrypt(message):
+    pass
 
-##def encrypt(message):
-##    pass
-##
-##
-##def send(encrypted_message):
-##    pass
-
+def send(encrypted_message):
+    pass
+"""
 
 
     
 
-## INITIALISEREN ##
-current_led = pyb.LED(2)    # deze moeten vervangen worden door pin_red_led en pin_infrared_led
-next_led = pyb.LED(3)       # voor het testen worden de LED's op het bordje gebruikt
 
 
-## CODE ##
-while True:
-    read_and_send()
-    delay(1000)
+## TEST CODE ##
 
-sprint('alles werkt')
+read_and_send()
+
+print('alles werkt')
 for i in range(10):
     pyb.LED(4).toggle()
     pyb.LED(1).toggle()
-    delay(200)
+    delay(150)
