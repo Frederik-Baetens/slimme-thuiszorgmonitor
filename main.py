f985726740502be8f139c43c1df1fd79327d5809
Tom-Bury <tom.bury@hotmail.com>
2016-10-20 17:56:44 +0200

    main aangepast
    globals proberen?

--- main.py
+++ main.py
@@ -1,97 +1,36 @@
-# info =>
-#
-# bemonsteren                 => Tom, Arnout
-#
-# 'encryptie'                => Siebe, Ruben
-#
-# => doorsturen (bluetooth)  => Jarno
-#
-# 'decryptie'
-
-## BRONNEN
-# http://docs.micropython.org/
-
-
-## INTERFACE
-# analoog_object = pyb.ADC(analoge_pin)           # create an analog object from a pin
-# val = analoog_object.read_timed(buffer, freq)   # read an analog value
-#                                                 # val = een pakktetje informatie
-#
-# adc = pyb.ADCAll(resolution)               # create an ADCAll object
-# val = analoog_object.read_channel(channel) # read the given channel
-# val = analoog_object.read_core_temp()      # read MCU temperature
-# val = analoog_object.read_core_vbat()      # read MCU VBAT
-# val = analoog_object.read_core_vref()      # read MCU VREF
-#
-#
-# adc = pyb.ADC(pyb.Pin.board.X19)    # create an ADC on pin X19
-# tim = pyb.Timer(6, freq=10)         # create a timer running at 10Hz
-# buf = bytearray(100)                # creat a buffer to store the samples
-# adc.read_timed(buf, tim)            # sample 100 values, taking 10s
-
-
-##INITIALIZATIE
-
-#Imports
+## IMPORTS ##
 import pyb
 
-#Constanten
-FREQ = 128              #frequentie waaraan gemeten wordt
-LENGHT_ELEMENT = 12
-LENGHTH_MESSAGE = 128
 
-#Pin nummers
-PO_PIN_NB = 'X4'              #nummer van de pulse-oximeter pin
-ECG_PIN_NB = 'X3'             #nummer van de electrocardiogram pin
-PRESSURE_PIN_NB = 'X5'        #nummer van de druksensor pin
+## CONSTANTEN ##
+FREQ = 128              #frequentie waaraan gemeten wordt
+LENGTH_ELEMENT = 12     #constanten voor omzetten naar binair
+LENGTH_MESSAGE = 128
+
+
+## PIN-NUMMERS ## --> deze pins zijn tijdelijk, kan nog gewijzigd worden
+PO_PIN_NB = 'X4'            #nummer van de pulse-oximeter pin
+ECG_PIN_NB = 'X3'           #nummer van de electrocardiogram pin
+PRESSURE_PIN_NB = 'X5'      #nummer van de druksensor pin
 PIN_RED_LED_NB = 'X1'       #X1: naam van de pin, naam van deze pin moet nog aangepast worden
 PIN_INFRARED_LED_NB = 'X2'  #X2: naam van de pin, naam van deze pin moet nog aangepast worden
 
-#Timer
-#mogelijke code voor timer, moet getest worden
-tim = pyb.Timer(4, freq=FREQ)
-tim.callback(read_and_send())
 
-#Naam geven aan de pinnen en sensoren 
+## PINNEN AANMAKEN ##
 po_pin = pyb.ADC(PO_PIN_NB)
 ecg_pin = pyb.ADC(ECG_PIN_NB)
 pressure_pin = pyb.ADC(PRESSURE_PIN_NB)
-pin_red_led = pyb.Pin(PIN_RED_LED_NB, Pin.OUT_PP)           #X1 (naam van de pin) aanpassen!
-pin_infrared_led = pyb.Pin(PIN_INFRARED_LED_NB, Pin.OUT_PP) #X2 (naam van de pin) aanpassen!
-accel = pyb.Accel()
-
-#Initializatie current_led en next_led
-#current_led = pin_red_led
-#next_led = pin_infrared_led
-current_led = pyb.LED(2)
-next_led = pyb.LED(3)
-
-##FUNCTIES
-
-def switch_leds(current_led,next_led):
-    #current_led.low()
-    #next_led.high()
-    #current_led, next_led = next_led, current_led
-    current_led.off()
-    next_led.on()
-    #current_led, next_led = next_led, current_led
-    #--> dit gaat niet werken: de variabelen van de functie worden omgewisseld (en niet erbuiten)
-    # deze omwisseling moet dus buiten deze functie gebeuren
-    
-def send(message):    #Jarno
-    pass
+##pin_red_led = pyb.Pin(PIN_RED_LED_NB, Pin.OUT_PP)           # deze code werkt nog niet, pins moeten ook aangepast worden
+##pin_infrared_led = pyb.Pin(PIN_INFRARED_LED_NB, Pin.OUT_PP) # deze code werkt nog niet
 
 
-def encrypt(message):
-    pass
-
-
+## FUNCTIES ##
 def reform_list(list):
-    #returns a message in binary that is 128 characters long
-    for i in len(list):
-        a[i] = str(bin(a[i]))
-    for i in range(len(list)):                                          
-        if len(list[i]) < LENGHT_ELEMENT:
+    # returns a message from a list with 3 elements in binary that is 128 characters long
+    for i in range(len(list)):
+        list[i] = bin(list[i])[2:]
+    for i in range(len(list)):
+        if len(list[i]) < LENGTH_ElEMENT:
             list[i] = (LENGTH_ELEMENT-len(list[i]))*'0' + list[i]
     message = ''
     for i in list:
@@ -102,19 +41,53 @@ def reform_list(list):
 
 
 def read_and_send():
+    # read the values of the sensor pins, switch the LEDs, encrypt the message, send the message via Bluetooth
     value_ecg = ecg_pin.read()
     value_po = po_pin.read()
     value_pressure = pressure_pin.read()
     switch_leds(current_led, next_led)
+    # LEDS verwisselen
+    current_led, next_led = next_led, current_led  
 
     list = [value_ecg, value_po, value_pressure]
     
-    message = list #reform_list(list)
-    print message
-    #encrypted_message = encrypt(message)
+    message = reform_list(list)
+    print(message)
+    
+    #ENCRYPTIE: encrypted_message = encrypt(message)
+    #BLUETOOTH: send(encrypted_message)
 
-    #send(encrypted_message) #Jarno
 
-##TEST CODE
+def switch_leds(led_on, led_off):
+    # led_on is the LED that is on before running this function (this will be turned off)
+    # led_off is the LED that is off before running this function (this will be turned on)
+    led_on.off()
+    led_off.on()
 
-read_and_send()
+
+##def encrypt(message):
+##    pass
+##
+##
+##def send(encrypted_message):
+##    pass
+
+
+
+    
+
+## INITIALISEREN ##
+current_led = pyb.LED(2)    # deze moeten vervangen worden door pin_red_led en pin_infrared_led
+next_led = pyb.LED(3)       # voor het testen worden de LED's op het bordje gebruikt
+
+
+## CODE ##
+while True:
+    read_and_send()
+    delay(1000)
+
+sprint('alles werkt')
+for i in range(10):
+    pyb.LED(4).toggle()
+    pyb.LED(1).toggle()
+    delay(200)
