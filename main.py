361e71456e3ac3c1a80d7e2b243d53d320ce9022
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-10-21 20:25:41 +0200

    Extra functie read gemaakt, list hernoemd naar lst, code properder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ## IMPORTS ##
-import pyb, time
+import pyb, utime
 
 
 ## CONSTANTEN ##
@@ -28,58 +28,59 @@ pin_infrared_led = pyb.LED(4)   #voorlopig voor test, moet een pin worden
 pin_red_led.on()
 pin_infrared_led.off()
 tim = pyb.Timer(1, freq=FREQ/128)   #frequentie moet gewoon FREQ worden
-sw.callback(lambda: switch_leds())  #probeersel voor interrupt
 sw = pyb.Switch()                   #misschien switch niet nodig
+sw.callback(lambda: switch_leds())  #probeersel voor interrupt
 
 ## FUNCTIES ##
-def reform_list(list):
+def reform_lst(lst):
     # returns a message from a list in binary that is 128 characters long
-    for i in range(len(list)):
-        list[i] = bin(list[i])[2:]
-    for i in range(len(list)):
-        if len(list[i]) < LENGTH_ELEMENT:
-            list[i] = (LENGTH_ELEMENT-len(list[i]))*'0' + list[i]
+    for i in range(len(lst)):
+        lst[i] = bin(lst[i])[2:]
+    for i in range(len(lst)):
+        if len(lst[i]) < LENGTH_ELEMENT:
+            lst[i] = (LENGTH_ELEMENT-len(lst[i]))*'0' + lst[i]
     message = ''
-    for i in list:
+    for i in lst:
         message += i
     if len(message) < LENGTH_MESSAGE:
         message += (LENGTH_MESSAGE-len(message))*'0'
     return message
 
-
-def read_and_send():
-    # read the values of the sensor pins, switch the LEDs, encrypt the message, send the message via Bluetooth
-    list = []
-    list.append(ecg_pin.read())
-    list.append(po_pin.read())
-    list.append(pressure_pin.read())
-    switch_leds() 
-    
-    message = reform_list(list)
-    print(message)
-    
-    #ENCRYPTIE: encrypted_message = encrypt(message)
-    #BLUETOOTH: send(encrypted_message)
-
+def read():
+    lst = []
+    lst.append(ecg_pin.read())
+    lst.append(po_pin.read())
+    lst.append(pressure_pin.read())
+    return lst 
 
 def switch_leds():
     pin_red_led.toggle()
     pin_infrared_led.toggle()
 
+def read_and_send():
+    # read the values of the sensor pins, switch the LEDs, encrypt the message, send the message via Bluetooth
+    switch_leds() 
+    lst=read()
+    message = reform_lst(lst)
+    print(message)
+    
+    #ENCRYPTIE: encrypted_message = encrypt(message)
+    #BLUETOOTH: send(encrypted_message)
 
 #timer, voorlopig voor tests
-def timer(function):
-    start=time.ticks_us()
-    function
-    stop=time.ticks_us()
-    return time.ticks_diff(start,stop)
+def timer():
+    start=utime.ticks_us()
+    reform_lst([3095,2021,3054])
+    return utime.ticks_diff(start,utime.ticks_us())
+
+print (timer())
+
 ##LOOP##
 # alles wat het bordje moet weten: functies variabelen etc moet hiervoor
 # wat hierna komt wordt nooit geevalueerd.
-while True:
+while False:
     if tim.counter()==0:
         read_and_send()
-
 
 
 """
