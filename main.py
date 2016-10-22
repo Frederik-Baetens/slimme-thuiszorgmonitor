7f76932d97b47f4f56a278a666fdf2d9cd38bc52
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-10-22 17:46:02 +0200

    aanpassing aan reform_lst

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 ## IMPORTS ##
-import pyb, utime
+import pyb, time
 
 
 ## CONSTANTEN ##
 FREQ = 128              #frequentie waaraan gemeten wordt
-LENGTH_ELEMENT = 12     #constanten voor omzetten naar binair
-LENGTH_MESSAGE = 128
 
 
 ## PIN-NUMMERS ##
@@ -27,6 +25,7 @@ pin_infrared_led = pyb.LED(4)   #voorlopig voor test, moet een pin worden
 ## INITIALIZATIE ##
 pin_red_led.on()
 pin_infrared_led.off()
+uart = pyb.UART(4,9600)
 tim = pyb.Timer(1, freq=FREQ/128)   #frequentie moet gewoon FREQ worden
 sw = pyb.Switch()                   #misschien switch niet nodig
 sw.callback(lambda: switch_leds())  #probeersel voor interrupt
@@ -35,7 +34,7 @@ sw.callback(lambda: switch_leds())  #probeersel voor interrupt
 
 def reform_lst(lst):
     #we laten 0b eraan hangen, zo kunnen stukken onderscheid worden
-    return ''.join([bin(i) for i in lst])
+    return ''.join([i for i in lst])
 
 def encrypt(lst):
     encryptedlst = lst  #voorlopige code
@@ -49,6 +48,7 @@ def read():
     return lst 
 
 def send(message):
+    uart.write(message)
     return
 
 def switch_leds():
@@ -69,9 +69,9 @@ def read_and_send():
 
 #timer, voorlopig voor tests
 def timer():
-    start=utime.ticks_us()
+    start=time.ticks_us()
     reform_lst([1515,4095,11])
-    return utime.ticks_diff(start,utime.ticks_us())
+    return time.ticks_diff(start,time.ticks_us())
 
 print (timer())
 
