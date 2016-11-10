06d2e04065a97c7abd076f2c5211a99613861e45
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-11-10 16:08:52 +0100

    code om een blok naar een lijst met constante lengte toegevoegd

--- hulpcode/pwm.py
+++ hulpcode/pwm.py
@@ -1,5 +1,3 @@
 import pyb 
-tim=pyb.Timer(2, freq=0.5) 
-ch=tim.channel(2, pyb.Timer.PWM, pin=pyb.Pin.board.X2) 
-ch.pulse_width_percent(50)
-~                             
+timer = pyb.Timer(2, freq=1)
+ch3 = timer.channel(3, pyb.Timer.PWM, pin=pyb.Pin.board.X3, pulse_width_percent=50)
--- hulpcode/serieel.py
+++ hulpcode/serieel.py
@@ -1,12 +1,14 @@
 import serial
 message=''
 ser=serial.Serial('/dev/rfcomm0',)
+f=open(
 while True:    
     data=str(ser.read(),'utf-8')
     if data!='.':
         message+=(data)
     else:
         print (message)
+
         message=''
 
 
