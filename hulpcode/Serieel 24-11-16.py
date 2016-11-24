b9bb9cdd15496d8c7de6982496d71b820d685cad
Tom-Bury <tom.bury@hotmail.com>
2016-11-24 17:18:53 +0100

    imports

--- hulpcode/Serieel 24-11-16.py
+++ hulpcode/Serieel 24-11-16.py
@@ -1,10 +1,30 @@
 import serial
+import Decryptie
+
+
 message=''
 decrypted_message=()
 ser=serial.Serial('/dev/rfcomm0')
 ekgfile = open('/home/frederik/Documents/ekgfile.txt','a')
 pofile = open ('/home/frederik/Documents/pofile.txt' ,'a')
 
+def dereform(string):
+    """
+    Maakt van de ontvangen vlakke string ('aaa.bbb.ccc. ... .zzz') opnieuw tupple van een lijst, een nummer en een lijst.
+    """
+
+    #herwerk de string tot een lijst van afzonderlijke waarden
+    list = []
+    check_new_point = 1
+    index_last_point = -1
+    while check_new_point < len(string):
+        if string[check_new_point] == '.':
+            list.append(int(string[index_last_point + 1:check_new_point]))
+            index_last_point = check_new_point
+        check_new_point += 1
+
+    list.append(int(string[index_last_point+1:-1]))
+    return list[:16], list[16], list[17:]
 
 while True:
     data = str(ser.read(),'utf-8')
@@ -14,7 +34,7 @@ while True:
             if data != ':':
                 message += data
             else:
-                decrypted_message = DECRYPT(dereform(message))
+                decrypted_message = Decryptie.Ontcijfering(dereform(message))
                 
                 for i in range(0, 8):
                     print(decrypted_message[i]) + '\n')
