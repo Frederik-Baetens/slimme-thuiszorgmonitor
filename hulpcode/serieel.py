2521c70e5a979b4158f9d1cd1b8d5190837e54bd
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-11-24 14:10:29 +0100

    oude serieel en nieuwe serieel aangemaakt

--- hulpcode/serieel.py
+++ hulpcode/serieel.py
@@ -1,21 +1,27 @@
 import serial
 message=''
-ser=serial.Serial('/dev/rfcomm0',)
+ser=serial.Serial('/dev/rfcomm0')
 ekgfile = open('/home/frederik/Documents/ekgfile.txt','a')
 pofile = open ('/home/frederik/Documents/pofile.txt' ,'a')
 filelijst = [ekgfile,pofile]
+position = 0
+filecounter = 0
 
 while True:
     data = str(ser.read(),'utf-8')
-    if data == ':'
-        filecounter = 0
-    elif data != '.':
-        message+=(data)
-    else: # als .
-        print (message)
-        message+='\n'
-        filelijst[filecounter].write(message)
-        filecounter = 1 - filecounter
-        message = ''
-
-
+    if data == ':':         #zorgt voor start met :
+        while True:
+            data = str(ser.read(),'utf-8')
+            if data != '.' and data != ':':
+                message += data
+            else:
+                print (message)
+                message += '\n'
+                filelijst[filecounter].write(message)
+                message = ''
+                position += 1
+                if position == 8:
+                    filecounter = 1
+                elif position == 12:
+                    filecounter = 0
+                    position = 0
