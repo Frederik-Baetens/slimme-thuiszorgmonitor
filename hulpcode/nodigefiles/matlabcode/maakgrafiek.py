2eadce472d7bac2f9a28762cb75bbf9a41a95590
Smurfendrek123 <Smurfendrek123@gmail.com>
2016-12-01 20:46:22 +0100

    grote kuis gedaan, serieeloud naar serieelpijpoud hernoemd

--- hulpcode/nodigefiles/matlabcode/maakgrafiek.py
+++
@@ -1,9 +0,0 @@
-import matplotlib
-matplotlib.use('TKAgg')
-import matplotlib.pyplot as plt
-f= open('datafile.txt','r')
-lines=f.readlines()
-f.close()
-
-plt.plot(lines)
-plt.show()
--- werkdirectory/maakgrafiek.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-f= open('ectgrafiek.txt','r')
-lines=f.readlines()
-f.close()
-
-plt.plot(lines)
-plt.show()
-
