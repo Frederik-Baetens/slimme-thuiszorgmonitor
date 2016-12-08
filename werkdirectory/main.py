58379dace64cd7bc26c22fb2d766097309ba25eb
smurfendrek123 <smurfendrek123@gmail.com>
2016-12-08 15:06:20 +0100

    retab

--- werkdirectory/main.py
+++ werkdirectory/main.py
@@ -3,23 +3,23 @@ import pyb, time, Encryptie
 
 
 ## CONSTANTEN ##
-FREQ = 120              #frequentie waaraan gemeten wordt
+FREQ = 120				#frequentie waaraan gemeten wordt
 NB_READINGS = 12
 NB_SENSORS = 2
 
 ## PIN-NUMMERS ##
-PO_PIN_NB = 'X19'           #nummer van de pulse-oximeter pin
-ECG_PIN_NB = 'X20'          #nummer van de electrocardiogram pin
-PIN_RED_LED_NB = 'X3'       #nummer van de rode led pin
-PIN_INFRARED_LED_NB = 'X4'  #nummer van de infrarode led pin
+PO_PIN_NB = 'X19'			#nummer van de pulse-oximeter pin
+ECG_PIN_NB = 'X20'			#nummer van de electrocardiogram pin
+PIN_RED_LED_NB = 'X3'		#nummer van de rode led pin
+PIN_INFRARED_LED_NB = 'X4'	#nummer van de infrarode led pin
 
 
 ## PINNEN AANMAKEN ##
 po_pin = pyb.ADC(PO_PIN_NB)
 ecg_pin = pyb.ADC(ECG_PIN_NB)
 
-pin_red_led = pyb.LED(3)        #moet weg, wordt nu met pwm gedaan
-pin_infrared_led = pyb.LED(4)   #voorlopig nog om leds te laten branden
+pin_red_led = pyb.LED(3)		#moet weg, wordt nu met pwm gedaan
+pin_infrared_led = pyb.LED(4)	#voorlopig nog om leds te laten branden
 
 
 ## INITIALISATIE ##
@@ -32,11 +32,11 @@ encryptie_counter = 0
 
 uart = pyb.UART(4, 1382400)
 tim1 = pyb.Timer(1, freq = FREQ)
-tim1.callback(lambda t: read(NB_READINGS))          #lezen
+tim1.callback(lambda t: read(NB_READINGS))			#lezen
 tim2 = pyb.Timer(2, freq = FREQ/NB_READINGS)
-tim2.callback(lambda t: uart.write(message))        #versturen
-tim7 = pyb.Timer(7, freq = FREQ/(NB_READINGS))    
-tim7.callback(lambda t: toggle_enable_reading())    #encrypt en reform
+tim2.callback(lambda t: uart.write(message))		#versturen
+tim7 = pyb.Timer(7, freq = FREQ/(NB_READINGS))	  
+tim7.callback(lambda t: toggle_enable_reading())	#encrypt en reform
 sw = pyb.Switch()
 sw.callback(lambda:pyb.LED(2).toggle())
 
@@ -54,48 +54,48 @@ def REFORM_LIST(tup):
 
 
 def encrypt(lst):
-    print (lst)
-    global encryptie_counter
-    encryptie_counter += 1
-    enclst = Encryptie.Vercijfering(encryptie_counter,lst)
-    global message
-    message = REFORM_LIST(enclst)
-    return
+	print (lst)
+	global encryptie_counter
+	encryptie_counter += 1
+	enclst = Encryptie.Vercijfering(encryptie_counter,lst)
+	global message
+	message = REFORM_LIST(enclst)
+	return
 
 def read(NB_READINGS):
-    global read_counter
+	global read_counter
 
-    lst_ecg[read_counter] = ecg_pin.read()//16
-    if read_counter % 3 == 0:
-        lst_po[read_counter//3] = po_pin.read()//16
+	lst_ecg[read_counter] = ecg_pin.read()//16
+	if read_counter % 3 == 0:
+		lst_po[read_counter//3] = po_pin.read()//16
 
-    if read_counter == NB_READINGS - 1:
-        read_counter = 0
-    else:
-        read_counter += 1
-    return
+	if read_counter == NB_READINGS - 1:
+		read_counter = 0
+	else:
+		read_counter += 1
+	return
 
 def switch_leds(): #moet weg
-    pin_red_led.toggle()
-    pin_infrared_led.toggle()
-    return
+	pin_red_led.toggle()
+	pin_infrared_led.toggle()
+	return
 
 def timer():
-    start = time.ticks_us()
-    #REFORM_LIST(([[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]],15,[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]))
-    Encryptie.Vercijfering(12,[255,255,1,0,255,110,211,0,0,255,144,255,0,5,10,240])
-    #read(NB_READINGS)
-    return time.ticks_diff(time.ticks_us(),start)
+	start = time.ticks_us()
+	#REFORM_LIST(([[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]],15,[[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]))
+	Encryptie.Vercijfering(12,[255,255,1,0,255,110,211,0,0,255,144,255,0,5,10,240])
+	#read(NB_READINGS)
+	return time.ticks_diff(time.ticks_us(),start)
 
 def toggle_enable_reading():
-    global enable_reading
-    enable_reading = 1
+	global enable_reading
+	enable_reading = 1
 
 ##LOOP##
 # alles wat het bordje moet weten: functies variabelen etc moet hiervoor
 # wat hierna komt wordt nooit geevalueerd.
 while True:
-    if enable_reading and read_counter == NB_READINGS-1 and pyb.Pin('A14').value() == 1:
-        enable_reading = 0
-        encrypt(lst_ecg + lst_po)
-        switch_leds()
+	if enable_reading and read_counter == NB_READINGS-1 and pyb.Pin('A14').value() == 1:
+		enable_reading = 0
+		encrypt(lst_ecg + lst_po)
+		switch_leds()
--- werkdirectory/main_zonder_enc.py
+++ werkdirectory/main_zonder_enc.py
@@ -3,24 +3,24 @@ import pyb, time
 
 
 ## CONSTANTEN ##
-FREQ = 128              #frequentie waaraan gemeten wordt
+FREQ = 128				#frequentie waaraan gemeten wordt
 NB_READINGS = 8
 NB_SENSORS = 2
 
 ## PIN-NUMMERS ##
-PO_PIN_NB = 'X19'           #nummer van de pulse-oximeter pin
-ECG_PIN_NB = 'X20'          #nummer van de electrocardiogram pin
-PRESSURE_PIN_NB = 'X21'     #nummer van de druksensor pin
-PIN_RED_LED_NB = 'X3'       #nummer van de rode led pin
-PIN_INFRARED_LED_NB = 'X4'  #nummer van de infrarode led pin
+PO_PIN_NB = 'X19'			#nummer van de pulse-oximeter pin
+ECG_PIN_NB = 'X20'			#nummer van de electrocardiogram pin
+PRESSURE_PIN_NB = 'X21'		#nummer van de druksensor pin
+PIN_RED_LED_NB = 'X3'		#nummer van de rode led pin
+PIN_INFRARED_LED_NB = 'X4'	#nummer van de infrarode led pin
 
 
 ## PINNEN AANMAKEN ##
 po_pin = pyb.ADC(PO_PIN_NB)
 ecg_pin = pyb.ADC(ECG_PIN_NB)
 pressure_pin = pyb.ADC(PRESSURE_PIN_NB)
-pin_red_led = pyb.LED(3)        #moet weg, wordt nu met pwm gedaan
-pin_infrared_led = pyb.LED(4)   #voorlopig nog om leds te laten branden
+pin_red_led = pyb.LED(3)		#moet weg, wordt nu met pwm gedaan
+pin_infrared_led = pyb.LED(4)	#voorlopig nog om leds te laten branden
 
 
 ## INITIALIZATIE ##
@@ -33,11 +33,11 @@ encryptie_counter = 0
 
 uart = pyb.UART(4, 1382400)
 tim1 = pyb.Timer(1, freq = FREQ)
-tim1.callback(lambda t: read(NB_READINGS))          #lezen
+tim1.callback(lambda t: read(NB_READINGS))			#lezen
 tim2 = pyb.Timer(2, freq = FREQ/NB_READINGS)
-tim2.callback(lambda t: uart.write(message))        #versturen
-tim7 = pyb.Timer(7, freq = FREQ/NB_READINGS)    
-tim7.callback(lambda t: toggle_enable_reading())    #encrypt en reform
+tim2.callback(lambda t: uart.write(message))		#versturen
+tim7 = pyb.Timer(7, freq = FREQ/NB_READINGS)	
+tim7.callback(lambda t: toggle_enable_reading())	#encrypt en reform
 sw = pyb.Switch()
 sw.callback(lambda:pyb.LED(2).toggle())
 
@@ -52,52 +52,52 @@ lstp = [0,]*(NB_READINGS//2)
 
 ## FUNCTIES ##
 def reform_list(tup):
-    #zet alle waarden uit een tuple: (lijst, getal, lijst)
-    #in 1 lage string, de waarden zijn gesplitst door punten met op het einde een dubbelpunt
-    return '.'.join([str(i) for i in tup])+':'
+	#zet alle waarden uit een tuple: (lijst, getal, lijst)
+	#in 1 lage string, de waarden zijn gesplitst door punten met op het einde een dubbelpunt
+	return '.'.join([str(i) for i in tup])+':'
 
 def encrypt(lst):
-    enclst = lst
-    print (lst)
-    global message
-    message = reform_list(enclst)
-    return
+	enclst = lst
+	print (lst)
+	global message
+	message = reform_list(enclst)
+	return
 
 def read(NB_READINGS):
-    global read_counter
-    lst[0+read_counter] = ecg_pin.read()
-    #if po_counter = 
-    lst[1+read_counter]= po_pin.read()
-    #lst[2+read_counter]= pressure_pin.read()
-    switch_leds()                       #overbodig, vervangen door pwm
-    if read_counter == (NB_READINGS-1)*NB_SENSORS:
-        read_counter = 0
-    else: read_counter += NB_SENSORS
-    return 
+	global read_counter
+	lst[0+read_counter] = ecg_pin.read()
+	#if po_counter = 
+	lst[1+read_counter]= po_pin.read()
+	#lst[2+read_counter]= pressure_pin.read()
+	switch_leds()						#overbodig, vervangen door pwm
+	if read_counter == (NB_READINGS-1)*NB_SENSORS:
+		read_counter = 0
+	else: read_counter += NB_SENSORS
+	return 
 
 def switch_leds():
-    pin_red_led.toggle()
-    pin_infrared_led.toggle()
-    return
+	pin_red_led.toggle()
+	pin_infrared_led.toggle()
+	return
 
 lijst1=[0,4095,1555,1489,2098,255,1023,1024]
 lijst2=[0,255,16,15]
 
 
 def timer():
-    start = time.ticks_us()
-    #reform_list(([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],15,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]))
-    #Encryptie.Vercijfering(12,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
-    return time.ticks_diff(time.ticks_us(),start)
+	start = time.ticks_us()
+	#reform_list(([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],15,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]))
+	#Encryptie.Vercijfering(12,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
+	return time.ticks_diff(time.ticks_us(),start)
 
 def toggle_enable_reading():
-    global enable_reading
-    enable_reading = 1
+	global enable_reading
+	enable_reading = 1
 
 ##LOOP##
 # alles wat het bordje moet weten: functies variabelen etc moet hiervoor
 # wat hierna komt wordt nooit geevalueerd.
 while True:
-    if enable_reading and read_counter==(NB_READINGS-1)*NB_SENSORS and pyb.Pin('A14').value() == 1:
-        enable_reading = 0
-        encrypt(lst)
+	if enable_reading and read_counter==(NB_READINGS-1)*NB_SENSORS and pyb.Pin('A14').value() == 1:
+		enable_reading = 0
+		encrypt(lst)
