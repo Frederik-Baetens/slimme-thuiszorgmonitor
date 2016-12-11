1e163bc907c52fd1fa00d31bc8518b0694dbb872
smurfendrek123 <smurfendrek123@gmail.com>
2016-12-11 22:27:14 +0100

    livegrafiek verder aangepast

--- werkdirectory/livegrafiek.py
+++ werkdirectory/livegrafiek.py
@@ -1,18 +1,40 @@
 import matplotlib, os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import numpy as np
 datafile = open('ekgpipe','r')
+
+fig, ax = plt.subplots()
+
+xlist = []
+ylist = []
+
+line, = ax.plot(xlist, ylist)
+
+def animate(i):
+	line.set_ydata(update())
+	xlist.append(i)
+	return line,
 
 def update():
 	data = (datafile.readline()).rstrip('\n')
 	return data
 
+def init():
+	line.set_ydata(np.ma.array(xlist, mask=True))
+	return line
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval = 100)
+
+plt.show()
+
+"""
 #plotting
 fig = plt.figure()
 ax = plt.axes(xlim=(0,600), ylim=(0,256))
-anim = animation.FuncAnimation(fig, update, interval=100)
+anim = animation.FuncAnimation(fig, update, fargs=None, interval=100)
 plt.show()
-
+"""
 
 """
 while True:
