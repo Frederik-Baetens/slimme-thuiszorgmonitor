5b27d6fe0f88079931c4dcd6741e83051702168b
smurfendrek123 <smurfendrek123@gmail.com>
2016-12-12 02:42:41 +0100

    files opgeruimd

--- hulpcode/nodigefiles/livegrafiek.py
+++ hulpcode/nodigefiles/livegrafiek.py
@@ -1,18 +1,62 @@
-import matplotlib, os
+import matplotlib, os, collections
+matplotlib.use('QT4AGG')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 datafile = open('ekgpipe','r')
 
-datalst=[]
-xlst=[]
-i=0
+NB=600
+MAX=256
 
-#plotten
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ylst = collections.deque([0]*NB,NB)
 
+fig, ax = plt.subplots()
+line, = ax.plot([0,]*NB)
+ax.set_ylim(0,MAX)
+
+
+
+def update(data):
+	line.set_ydata(data)
+	return line,
+
+def data_gen():
+	while True:
+		ylst.append(eval((datafile.readline()).rstrip('\n')))
+		yield ylst
+
+
+ani = animation.FuncAnimation(fig,update,data_gen,interval=0, blit=True)
+plt.show()
+
+"""
 def animate(i):
-	dataArray = 
+	line.set_ydata(update(i)[0])
+	line.set_xdata(update(i)[1])
+	ylist.append(update(i)[0])
+	xlist.append(update(i)[1])
+	return line,
+
+def update():
+	data = (datafile.readline()).rstrip('\n')
+	return data
+
+def init():
+	line.set_ydata(np.ma.array(xlist, mask=True))
+	return line
+
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval = 100)
+
+plt.show()
+"""
+
+"""
+#plotting
+fig = plt.figure()
+ax = plt.axes(xlim=(0,600), ylim=(0,256))
+anim = animation.FuncAnimation(fig, update, fargs=None, interval=100)
+plt.show()
+"""
+
 """
 while True:
 	xlst.append(i)
