9581552465b416ee76a57617d11ef2a2f4aaa08c
smurfendrek123 <smurfendrek123@gmail.com>
2016-12-18 15:36:35 +0100

    livegrafiek sneller gemaakt

--- data/maakgrafiek.py
+++ data/maakgrafiek.py
@@ -1,10 +1,14 @@
 import matplotlib
+matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
+
 datafile = input('datafile: ')
 f= open(datafile,'r')
 lines1=f.readlines()
 f.close()
 lines2=[eval(i)//16 for i in lines1]
 
-plt.plot(lines1)
+plt.plot(lines2)
+plt.ylabel('waarde')
+plt.xlabel('samples')
 plt.show()
--- werkdirectory/livegrafiek.py
+++ werkdirectory/livegrafiek.py
@@ -1,8 +1,8 @@
 import matplotlib, os, collections
-matplotlib.use('QT4AGG')
+matplotlib.use('qt4agg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-datafile = open('popipe','r')
+datafile = open('ekgpipe','r')
 
 NB=600
 MAX=256
@@ -16,13 +16,14 @@ ax.set_ylim(0,MAX)
 
 
 def update(data):
-	line.set_ydata(data)
-	return line,
+        line.set_ydata(data)
+        return line,
 
 def data_gen():
-	while True:
-		ylst.append(eval((datafile.readline()).rstrip('\n')))
-		yield ylst
+        while True:
+                ylst.append(eval((datafile.readline()).rstrip('\n')))
+                ylst.append(eval((datafile.readline()).rstrip('\n')))
+                yield ylst
 
 
 ani = animation.FuncAnimation(fig,update,data_gen,interval=0, blit=True)
@@ -30,19 +31,19 @@ plt.show()
 
 """
 def animate(i):
-	line.set_ydata(update(i)[0])
-	line.set_xdata(update(i)[1])
-	ylist.append(update(i)[0])
-	xlist.append(update(i)[1])
-	return line,
+        line.set_ydata(update(i)[0])
+        line.set_xdata(update(i)[1])
+        ylist.append(update(i)[0])
+        xlist.append(update(i)[1])
+        return line,
 
 def update():
-	data = (datafile.readline()).rstrip('\n')
-	return data
+        data = (datafile.readline()).rstrip('\n')
+        return data
 
 def init():
-	line.set_ydata(np.ma.array(xlist, mask=True))
-	return line
+        line.set_ydata(np.ma.array(xlist, mask=True))
+        return line
 
 ani = animation.FuncAnimation(fig, animate, init_func=init, interval = 100)
 
@@ -59,11 +60,11 @@ plt.show()
 
 """
 while True:
-	xlst.append(i)
-	i+=1
+        xlst.append(i)
+        i+=1
 
-	datalst.append((datafile.readline()).rstrip('\n'))
-	
-	plt.plot(xlst,datalst)
-	plt.pause(0.005)
+        datalst.append((datafile.readline()).rstrip('\n'))
+        
+        plt.plot(xlst,datalst)
+        plt.pause(0.005)
 """
